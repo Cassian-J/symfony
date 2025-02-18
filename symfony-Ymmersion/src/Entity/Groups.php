e3be002a2b295c7f980dfa7bf0bf7bcfd1ebb8c9
cassian joly <cassian.joly@ynov.com>
2025-02-18 16:37:03 +0100

    recreate all the entity with foreign key

---
+++ symfony-Ymmersion/src/Entity/Groups.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GroupsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GroupsRepository::class)]
+class Groups
+{
+    #[ORM\Id]
+    #[ORM\Column(type: Types::GUID ,nullable:false)]
+    #[ORM\GeneratedValue(strategy: "NONE")] // UUID généré manuellement
+    private ?string $GroupUuid = null;
+
+    #[ORM\Column(type: Types::TEXT,nullable:false)]
+    private ?string $Name = null;
+
+    #[ORM\Column(type: Types::INTEGER,nullable:false)]
+    private ?int $Point = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'],targetEntity: Users::class)]
+    #[ORM\JoinColumn(nullable:false ,name: 'Creator', referencedColumnName: 'UserUuid', unique: true)]
+    private ?Users $Creator = null;
+
+
+    public function getGroupUuid(): ?string
+    {
+        return $this->GroupUuid;
+    }
+
+    public function setGroupUuid(string $GroupUuid): static
+    {
+        $this->GroupUuid = $GroupUuid;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): static
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getPoint(): ?int
+    {
+        return $this->Point;
+    }
+
+    public function setPoint(int $Point): static
+    {
+        $this->Point = $Point;
+
+        return $this;
+    }
+
+    public function getCreator(): ?Users
+    {
+        return $this->Creator;
+    }
+
+    public function setCreator(Users $Creator): static
+    {
+        $this->Creator = $Creator;
+
+        return $this;
+    }
+}
---
+++ symfony-Ymmersion/src/Entity/Users.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsersRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UsersRepository::class)]
+class Users
+{
+    #[ORM\Id]
+    #[ORM\Column(type: Types::GUID,nullable:false)]
+    #[ORM\GeneratedValue(strategy: "NONE")] // UUID généré manuellement
+    private ?string $UserUuid = null;
+
+    #[ORM\Column(type: Types::TEXT, unique: true,nullable:false)]
+    private ?string $Email = null;
+
+    #[ORM\Column(type: Types::TEXT, unique: true,nullable:false)]
+    private ?string $Pseudo = null;
+
+    #[ORM\Column(type: Types::TEXT,nullable:false)]
+    private ?string $Pwd = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE,nullable:false)]
+    private ?\DateTimeInterface $lastConnection = null;
+
+    #[ORM\ManyToOne(targetEntity: Groups::class)]
+    #[ORM\JoinColumn(name: 'GroupUuid', referencedColumnName: 'GroupUuid', nullable: true)]
+    private ?Groups $GroupUuid = null;
+
+    public function getUserUuid(): ?string
+    {
+        return $this->UserUuid;
+    }
+
+    public function setUserUuid(string $UserUuid): static
+    {
+        $this->UserUuid = $UserUuid;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): static
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->Pseudo;
+    }
+
+    public function setPseudo(string $Pseudo): static
+    {
+        $this->Pseudo = $Pseudo;
+
+        return $this;
+    }
+
+    public function getPwd(): ?string
+    {
+        return $this->Pwd;
+    }
+
+    public function setPwd(string $Pwd): static
+    {
+        $this->Pwd = $Pwd;
+
+        return $this;
+    }
+
+    public function getLastConnection(): ?\DateTimeInterface
+    {
+        return $this->lastConnection;
+    }
+
+    public function setLastConnection(\DateTimeInterface $lastConnection): static
+    {
+        $this->lastConnection = $lastConnection;
+
+        return $this;
+    }
+
+    public function getGroupUuid(): ?Groups
+    {
+        return $this->GroupUuid;
+    }
+
+    public function setGroupUuid(?Groups $GroupUuid): static
+    {
+        $this->GroupUuid = $GroupUuid;
+
+        return $this;
+    }
+}

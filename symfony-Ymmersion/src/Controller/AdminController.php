71c284d401f0b17babb7dba99865ad039db52997
Louka-Gennies <146700433+Louka-Gennies@users.noreply.github.com>
2025-02-18 08:50:41 +0100

    ❎  disable the mail verification

--- symfony-Ymmersion/src/Controller/AdminController.php
+++ symfony-Ymmersion/src/Controller/AdminController.php
@@ -11,14 +11,15 @@ final class AdminController extends AbstractController
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
-        $this->denyAccessUnlessGranted("IS_AUTHENTICATED_FULLY");
+        # $this->denyAccessUnlessGranted("IS_AUTHENTICATED_FULLY");
 
         /** @var User $user */
         $user = $this->getUser();
 
         return match ($user->isVerified()) {
             true => $this->render('admin/index.html.twig'),
-            false => $this->render('admin/please-verify-email.html.twig'),
+            false => $this->render('admin/index.html.twig'),
+            # false => $this->render('admin/please-verify-email.html.twig'),
         };
         return $this->render('admin/index.html.twig');
     }

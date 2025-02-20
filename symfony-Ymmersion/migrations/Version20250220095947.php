29c94fad3b20a24e61c73886456b79bc9782686e
cassian joly <cassian.joly@ynov.com>
2025-02-20 11:01:03 +0100

    made the groups page

--- symfony-Ymmersion/migrations/Version20250220095947.php
+++ symfony-Ymmersion/migrations/Version20250220095947.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250220081613 extends AbstractMigration
+final class Version20250220095947 extends AbstractMigration
 {
     public function getDescription(): string
     {
